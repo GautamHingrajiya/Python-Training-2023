b06694ec04d8b44701def8a63cad357f2c02f71c
GautamHingrajiya <137255545+GautamHingrajiya@users.noreply.github.com>
2023-07-06 12:42:40 +0530

    push

--- Class Practice/collection/menu_dictionary.py
+++ Class Practice/collection/menu_dictionary.py
@@ -14,28 +14,27 @@ menu = {
 }
 print(menu1)
 bill_amt = 0
-
-for i in range(0,len(menu)):
-    item_name = input("Eter Item Want to Oreder : ")
-    item_qty =  int(input("Eter Item Want to Qty : "))
-    if item_name in menu:
-        bill_amt = item_qty * menu.get(item_name)
-        print(bill_amt)
-    else:
-        print
+total_bill=0
+status = True
+while status:
     
+    for i in range(0,len(menu)):
+        
+        item_name = input("Enter Item Want to Oreder : ")
+        item_qty =  int(input("Enter Item Want to Qty : "))
 
-
-
-
-# for i in range (1,len(quiz)+1):
-#     print(f"Que. {i} {quiz[i]['Que']} ")
+        if item_name in menu:
+            bill_amt = item_qty * menu.get(item_name)
+            print("bill_amt : ",bill_amt)
+            total_bill +=bill_amt         
+            order_more = input("Want to add More Item(y/n) : ").upper()
+            if order_more == "Y" or order_more == "YES":
+                    continue
+            else:
+                    status =False
+                    break  
+        else:
+            print("Enter Valid Name")
+         
+    print("TOTAL BILL AMT : ",total_bill)
     
-#     ans = input("Enter Your Ans : ").lower()
-#     if quiz[i]['ans'] == ans:
-#         score+=50
-#         print("Right Answer")
-#     else:
-#         score-=20
-#         print("wrong Answer")
-# print(f"score = {score}")

a8978c4d731e5ad0a3e0964b6bc58da55e903538
GautamHingrajiya <137255545+GautamHingrajiya@users.noreply.github.com>
2023-08-01 10:07:59 +0530

    push

---
+++ Class Practice/tkinter/01.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("980x620")
+root.title("Billing Softwear")
+root.minsize(800,600)
+root.maxsize(1366,768)
+lable1= Label(text="Billing Softwear")
+lable1.pack()
+
+# photo = PhotoImage(file="C:/Users/Gautam/Documents/GitHub/Python-Training-2023/Class Practice/tkinter/Desktop_Wallpapers.png")
+# Photo_lable1 = Label(image = photo)
+# Photo_lable1.pack() 
+root.mainloop()
